45b6a6416ba444b94b0443265e03d6593e5590a5
surrealegg <surrealegg@pm.me>
2022-01-28 23:51:46 +0400

    Add processRetry

--- typings/rpgmakermv/rpg_managers.d.ts
+++ typings/rpgmakermv/rpg_managers.d.ts
@@ -1124,6 +1124,7 @@ declare class BattleManager {
   static _escaped: boolean;
   static _rewards: IDataRewards;
 
+  static processRetry(): void;
   static refreshEnemyReinforcements(): void;
   static setup(troopId: number, canEscape: boolean, canLose: boolean): void;
   static initMembers(): void;
